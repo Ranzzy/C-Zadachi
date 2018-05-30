9bbe54a1aa2d19e817f029358f57fd68bde45892
Ranzzy <mark1907@mail.ru>
2018-05-30 10:19:55 +0700

    12

--- 4.cpp
+++ 4.cpp
@@ -3,6 +3,16 @@
 using namespace std;
 
 int main() {
-    
+    int arr[10];
+    for (int i=0; i<10; i++){
+        cout << "[" << i+1 << "]" << ":";
+        cin >> arr[i];
+    }
+
+    cout << "Массив: ";
+    for (int i=0; i<10; ++i) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
     return 0;
 }
